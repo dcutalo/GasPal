a0f4733fb321ce804ac368abc3fff38380958277
Dean Cutalo <diesel31499@gmail.com>
2021-10-07 15:39:47 -0400

    Added crud methods for users data

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ app.get('/g', (req, res) => {
     res.send('Goodbye!')
 })
 
+app.get('/users', (req, res) => {
+    return res.send('GET HTTP method on user');
+});
+
+app.post('/users', (req, res) => {
+    return res.send('POST HTTP method on user');
+});
+
+app.put('/users', (req, res) => {
+    return res.send('PUT HTTP method on user');
+});
+
+app.delete('/users', (req, res) => {
+    return res.send('DELETE HTTP method on user');
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
