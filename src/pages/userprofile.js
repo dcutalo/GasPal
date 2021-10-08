bc068eae2d40f7cb23b1dbe4f4caf80a0ba4cd26
unknown <jun95987@yahoo.com>
2021-10-08 11:59:46 -0400

    layout

---
+++ src/pages/userprofile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserProfile() {
+    return (
+        <div className='userprofile'>
+            <h1>UserProfile</h1>
+        </div>
+    )
+}
+
+export default UserProfile
