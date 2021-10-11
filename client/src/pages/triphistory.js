05d636c37567edd408cca87f0dbc9929a5b6e716
MickColours <mccull88@students.rowan.edu>
2021-10-10 23:13:43 -0400

    Reworked the express server connection with newly introduced API nodes and made Express to React connection on tripHistory

--- client/src/pages/triphistory.js
+++ client/src/pages/triphistory.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import '../style/triphistory.css'
+class TripHistory extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tripSet: []
+        };
+    }
 
-function TripHistory() {
-    return (
-        <div className='triphistory'>
-            <h1>Triphistory</h1>
-        </div>
-    )
+    componentDidMount() {
+        console.log("grabbing has commenced")
+        fetch("/trips/").then(res => res.text()).then(tripSet => this.setState({tripSet}, () => console.log('Trips grabbed:', tripSet)))
+    }
+
+    render() {
+        return (
+            <div className='triphistory'>
+                <h1>Current User Trip History</h1>
+                <br></br>
+                    {
+                        
+                    /* {this.state.tripSet.map(function(listValue) {return <li>{listValue}</li>;})} //STUBBED OUT UNTIL DEVELOPMENT FOR JSON HANDLING HAS FINISHED */
+                        <li class="tripList">{this.state.tripSet}</li>
+                    }
+            </div>
+        );
+    }
 }
+
+
 
 export default TripHistory
