9effc7478de66b8f2b5b191fa4b4af8a68c6a6a2
Dean Cutalo <diesel31499@gmail.com>
2021-10-11 16:03:15 -0400

    Added more to the api with insert and delete functions

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ connection = connectionRequest();
     console.log(rows)
 })*/
 
+//trip table (needs post, put, delete)
 app.get('/trips/:user', (req, res) => {
     const queryString = "SELECT * from trip WHERE username = ?"
     connection.query(queryString, [req.params.user], function (err, rows, fields) {
@@ -29,30 +30,101 @@ app.get('/trips', (req, res) => {
         return res.json(rows);
     })
 });
+//end
+
+//user table (needs put)
+app.get('/users/:username', (req, res) => {
+    const queryString = "SELECT * from user WHERE username = ?"
+    connection.query(queryString, [req.params.username], function (err, rows, fields) {
+
+        if (err) throw err
+
+        return res.json(rows);
+    })
+});
+//insert
+app.get('/users/:username/:email', (req, res) => {
+    const queryString = "INSERT INTO user (username, email) VALUES (?,?);"
+    connection.query(queryString, [req.params.username, req.params.email], function (err, rows, fields) {
+            if (err) {
+                throw err
+            }
+            else {
+                return res.json('Insert succesful');
+            }
+        })
+});
+//delete
+app.get('/userdelete/:username', (req, res) => {
+    const queryString = "DELETE FROM user WHERE username = ?"
+    connection.query(queryString, [req.params.username], function (err, rows,fields) {
+            if (err) {
+                throw err
+            }
+            else {
+                return res.json('Succesfully removed user');
+            }
+    })
+});
+
+app.get('/users', (req, res) => {
+    const queryString = "SELECT * from user"
+    connection.query(queryString, function (err, rows, fields) {
+
+        if (err) throw err
+
+        return res.json(rows);
+    })
+});
+//end
+
+//car table (needs post, put, delete)
+app.get('/cars/:car_id', (req, res) => {
+    const queryString = "SELECT * from car WHERE car_id = ?"
+    connection.query(queryString, [req.params.car_id], function (err, rows, fields) {
+        
+        if (err) throw err
+
+        return res.json(rows);
+    })
+});
+
+app.get('/cars', (req, res) => {
+    const queryString = "Select * from car"
+    connection.query(queryString, function (err, rows, fields) {
+
+        if (err) throw err
+
+        return res.json(rows);
+    })
+});
+// end
+
+//user_owned_car (needs post, put, delete)
+app.get('/usercars/:username', (req, res) => {
+    const queryString = "SELECT * from user_owned_car WHERE username = ?"
+    connection.query(queryString, [req.params.username], function (err, rows, fields) {
+
+        if (err) throw err
+
+        return res.json(rows);
+    })
+});
+
+app.get('/usercars', (req, res) => {
+    const queryString = "Select * from user_owned_car"
+    connection.query(queryString, function (err, rows, fields) {
+
+        if (err) throw err
+
+        return res.json(rows);
+    })
+});
+//end
 
 app.get('/', (req, res) => {
     res.send('Hello!')
 })
-
-app.get('/g', (req, res) => {
-    res.send('Goodbye!')
-})
-
-app.get('/users', (req, res) => {
-    return res.send('GET HTTP method on user');
-});
-
-app.post('/users', (req, res) => {
-    return res.send('POST HTTP method on user');
-});
-
-app.put('/users', (req, res) => {
-    return res.send('PUT HTTP method on user');
-});
-
-app.delete('/users', (req, res) => {
-    return res.send('DELETE HTTP method on user');
-});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
