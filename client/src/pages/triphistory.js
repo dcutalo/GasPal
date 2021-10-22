1c6374a7736b27a880523e0074526b62a14ce370
unknown <jun95987@yahoo.com>
2021-10-22 10:04:55 -0400

    Added responsive gas gauge on Home page

--- client/src/pages/triphistory.js
+++ client/src/pages/triphistory.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import '../style/triphistory.css';
 import Axios from 'axios';
+const name = "coolguy04"
 
 function TripHistory() {
 
     // const [itemname, methodcall] = useState(itemtype)
     const [tripList, setTripList] = useState([])
-
+    const [car, setCar] = useState([])
+    
     // use AXIOS to communicate with backend
     useEffect(() => {
         Axios.get("http://localhost:5000/trips").then((response) => 
@@ -15,21 +17,33 @@ function TripHistory() {
         });
     }, []);
 
+    useEffect(() => {
+        Axios.get("http://localhost:5000/cars").then((response) => 
+        {
+            setCar(response.data);
+        });
+    }, []);
+    
+    
     
     return (
         <div className='triphistory'>
             <h1 className="tripHisHeader">Current User Trip History</h1>
             <br></br>
-            {tripList.map((val) =>{
-                return (
-                <h2 className="tripinfo">
-                    Trip_ID: {val.trip_id}  Car_ID: {val.car_id}<br></br>
-                    Starting Address: {val.start_adr} <br></br>
-                    Destination Address: {val.end_adr}<br></br>
-                    Distance: {val.distance} 
-                </h2>
-                );
-            })};
+            {tripList.map((val) => 
+                car.map((val2) =>{
+                    if (val.car_id == val2.car_id){
+                        return (
+                        <h2 className="tripinfo">
+                            make: {val2.make} <br></br>
+                            Trip_ID: {val.trip_id}  Car_ID: {val.car_id}<br></br>
+                            Starting Address: {val.start_adr} <br></br>
+                            Destination Address: {val.end_adr}<br></br>
+                            Distance: {val.distance} 
+                        </h2>
+                    )}
+                })
+            )};
         </div>
         );
 }
